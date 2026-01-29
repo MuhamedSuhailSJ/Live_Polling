85b00839e71fe59c2fa55b52cf454a9b4c9689a2
MuhamedSuhail <s.j.muhamedsuhail@goutlook.in>
2026-01-30 00:07:28 +0530

    url added

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://live-polling-opal.vercel.app"; // Or process.env.REACT_APP_API_URL
+const BASE_URL = "https://live-polling-opal.vercel.app/"; 
 
 export const api = {
   createPoll: async () => {
